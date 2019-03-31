57f9fbacc8555caf936b947e332e1c7237313c40
Louise Iyengar <lki@louiseiyengar.com>
2019-03-30 23:19:37 -0400

    Re-organize code

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  /** 
-    Fetch Functions
-  */
 
+  /** 
+   * CLOBAL CONSTANTS
+  */
   let employees = new Array();
   let searchEmployees = new Array();
-  
+ 
+  /** 
+    FETCH PROMISE
+  */
   fetch('https://randomuser.me/api/1.2/?format=json&results=12&nat=au,gb,nz,us')
     .then(checkStatus)
     .then(res => res.json())
     .then(data => {
       employees = data.results;
       createEmployeeCards(employees);
+      createModalContainer();
       createSearchForm();
     })
     .catch(error => console.log('looks like there was a problem', error)
   );  //end fetch
 
- 
+  /** 
+    EVENT LISTENERS
+  */
   document.addEventListener('click', e => {
-    //close button on modal click
+    //close button on modal
     if ((e.target.parentNode.className === "modal-close-btn") || (e.target.className === 'modal-close-btn')) {
       document.getElementsByClassName('modal-container')[0].style.display = "none"; 
       if (searchEmployees.length) {
@@ -36,6 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
       addModalInfo(employee);
       document.getElementsByClassName('modal-container')[0].style.display = "block"; 
 
+    //change to previous employee if 'Prev' button clicked on modal
     } else if (e.target.id === 'modal-prev') {
       if (searchEmployees.length > 0) {
         processNextPrev(searchEmployees, "prev")
@@ -43,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
         processNextPrev(employees, "prev");
       }
 
+    //change to next employee if 'Prev' button clicked on modal
     } else if (e.target.id === 'modal-next') {
       if (searchEmployees.length > 0) {
         processNextPrev(searchEmployees, "next")
@@ -50,146 +58,61 @@ document.addEventListener('DOMContentLoaded', () => {
         processNextPrev(employees, "next");
       }
     } 
-  }); //end event listener
+  }); //end CLICK event listener
 
+  //Process Search if Search Form is submitted
   document.addEventListener('submit', e => {
     e.preventDefault();
     processSearch(e.target.childNodes[0].value);
   });
 
   /** 
-    Helper Functions
+    FUNCTIONS TO CREATE EMPLOYEE CARDS AND MODAL CARD
   */
-  function processNextPrev(employees, which) {
-    indexOfCard = getCardIndex(employees);
-    if (which === "next") {
-      indexOfCard = (indexOfCard === employees.length - 1) ? 0 : (indexOfCard + 1);
-    } else {
-      indexOfCard = (indexOfCard) ? (indexOfCard - 1) : (employees.length - 1);
-    }
-    addModalInfo(employees[indexOfCard]);
-  }
+  function createEmployeeCards(employees) {
+    const galleryDiv = document.getElementById('gallery');
 
-  function checkStatus(response) {
-    if (response.ok) {
-      return Promise.resolve(response);
-    } else {
-      return Promise.reject(new Error(response.statusText));
-    }
-  }
-
-  function getCountry(employeeCountry) {
-    const countryArray = [
-      {abbr: "AU", country: "Australia"}, {abbr: "GB",country: "United Kingdom"},
-      {abbr: "NZ", country: "New Zealand"}, {abbr: "US",country: "United States"}
-    ];
-    return countryArray.find(country => employeeCountry === country.abbr).country;
-  }
-
-  // https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
-
-  function formatPhoneNumber(phoneNumberString) {
-    if (phoneNumberString.charAt(4) === "-" || phoneNumberString.charAt(4) === ")") {
-      var cleaned = (phoneNumberString).replace(/\D/g, '')
-      var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
-      if (match) {
-        return '(' + match[1] + ') ' + match[2] + '-' + match[3]
-      }
-    }
-    return phoneNumberString;
-  }
-
-  function formatBirthday(birthday) {
-    birthday = new Date(birthday);
-
-    return (birthday.getMonth() + 1) + "/" + birthday.getDate() + "/" + birthday.getFullYear();
-  }
-
-  function getCardIndex(employeeArray) {
-    const targetEmail = document.getElementsByClassName("modal-text")[0].textContent;
-    return employeeArray.findIndex((employee => employee.email === targetEmail));
-  }
-
-  function resetAllEmployees() {
-    if (document.getElementById("search-input").value === '') {
-      //remove 'no search result' error message if exists
-      const searchContainer = document.getElementsByClassName('search-container')[0];
-      const errorDiv = document.getElementsByClassName('search-error');
-      if (errorDiv.length) {
-        searchContainer.removeChild(errorDiv[0]);
-      }
-      
-      //Display all employees and reset searchEmployees array
-      if (searchEmployees.length > 0) {
-        createEmployeeCards(employees);
-        searchEmployees = [];
-      }
-    }
-  }
-
-  function createSearchForm() {
-    const searchDiv = document.getElementsByClassName("search-container")[0];
-    const searchForm  = document.createElement("FORM");
-    searchForm.setAttribute('method',"get");
-    searchForm.setAttribute('action',"#");
-    searchDiv.appendChild(searchForm);
-    searchForm.innerHTML = '<input type="search" id="search-input" class="search-input" placeholder="Search...">';
-    searchForm.innerHTML += '<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">';
-
-    document.getElementById("search-input").addEventListener('click', function () {
-      setTimeout(function() {
-        resetAllEmployees();
-       }, 20)
-    });
-
-    document.getElementById("search-input").addEventListener('keyup', function () {
-      resetAllEmployees();
-    });
-  
-  }
-
-function createSearchArray (userInput) {
-  //  let searchEmployees = new Array();
-    searchEmployees = [];
-    //CREATE ARRAY OF FOUND STUDENTS -- push li elements for students that match user input
-    //if user enters more than one space between characters, ensure only one space
-    userInput = userInput.trim().toLowerCase().replace(/  +/g, ' ');   
+    galleryDiv.innerHTML = '';
 
     employees.forEach(employee => {
-      let name = employee.name.first + " " + employee.name.last;
-      fName = employee.name.first.trim().toLowerCase();
-      lName = employee.name.last.trim().toLowerCase(); //if more than one space in rest of name (ie. lilou le gall)
-
-       if ((name.search(userInput) === 0) 
-       || (fName.search(userInput) === 0)
-       || (lName.search(userInput) === 0)) {
-            searchEmployees.push(employee);
-       }
+      const employeeCard = `
+        <div class="card">
+          <div class="card-img-container">
+              <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
+          </div>
+          <div class="card-info-container">
+              <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+              <p class="card-text">${employee.email}</p>
+              <p class="card-text cap">${employee.location.state}, ${getCountry(employee.nat)}</p>
+          </div>
+        </div>
+      `
+      galleryDiv.innerHTML += employeeCard;
     });
-    return searchEmployees;
- }
-
-  function processSearch(userInput) {
-    searchEmployees = createSearchArray(userInput);
-    if (searchEmployees.length) {
-      createEmployeeCards(searchEmployees);
-    } else {
-      processNoResults();
-    }
   }
 
-  function processNoResults() {
-    const divSearchContainer = document.getElementsByClassName('search-container')[0];
-    if (document.getElementsByClassName('search-error').length === 0) {
-      const divError = document.createElement('DIV');
-      divError.classList.add('search-error');
-      divError.innerHTML = '<p>Your search found no results</p>';
-      divSearchContainer.appendChild(divError);
-    }
-    createEmployeeCards(employees);
-    searchEmployees = [];
+  function addModalInfo(employee) {
+    const infoContainer = document.getElementsByClassName("modal-info-container")[0];
+    const country = getCountry(employee.nat);
+
+    const employeeInfo = `
+      <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+      <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+      <p class="modal-text">${employee.email}</p>
+      <p class="modal-text cap">${employee.location.state}, ${country}</p>
+      <hr />
+      <p class="modal-text">${formatPhoneNumber(employee.phone)}</p>
+      <p class="modal-text cap">${employee.location.street}</p>
+      <p class="modal-text cap">${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+      <p class="modal-text cap">${country}</p>
+      <p class="modal-text">Birthday: ${formatBirthday(employee.dob.date)}</p>
+    `;
+     infoContainer.innerHTML = employeeInfo;
   }
 
+  /** 
+    FUNCTION TO CREATE MODAL 
+  */
   function createModalContainer() {
     const body = document.querySelector('body');
     const galleryNextSibling = document.getElementById('gallery').nextElementSibling;
@@ -223,51 +146,134 @@ function createSearchArray (userInput) {
   }
 
   /** 
-    Create Cards and Modal
+    FUNCTIONS TO PROCESS SEARCH
   */
-
-  function addModalInfo(employee) {
-    const infoContainer = document.getElementsByClassName("modal-info-container")[0];
-    const country = getCountry(employee.nat);
-
-    const employeeInfo = `
-      <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-      <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-      <p class="modal-text">${employee.email}</p>
-      <p class="modal-text cap">${employee.location.state}, ${country}</p>
-      <hr />
-      <p class="modal-text">${formatPhoneNumber(employee.phone)}</p>
-      <p class="modal-text cap">${employee.location.street}</p>
-      <p class="modal-text cap">${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-      <p class="modal-text cap">${country}</p>
-      <p class="modal-text">Birthday: ${formatBirthday(employee.dob.date)}</p>
-    `;
-     infoContainer.innerHTML = employeeInfo;
+  function processSearch(userInput) {
+    searchEmployees = createSearchArray(userInput);
+    if (searchEmployees.length) {
+      createEmployeeCards(searchEmployees);
+    } else {
+      processNoResults();
+    }
   }
 
-  
-  function createEmployeeCards(employees) {
-    const galleryDiv = document.getElementById('gallery');
+  function createSearchForm() {
+    const searchDiv = document.getElementsByClassName("search-container")[0];
+    const searchForm  = document.createElement("FORM");
+    searchForm.setAttribute('method',"get");
+    searchForm.setAttribute('action',"#");
+    searchDiv.appendChild(searchForm);
+    searchForm.innerHTML = '<input type="search" id="search-input" class="search-input" placeholder="Search...">';
+    searchForm.innerHTML += '<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">';
 
-    galleryDiv.innerHTML = '';
-
-    employees.forEach(employee => {
-      const employeeCard = `
-        <div class="card">
-          <div class="card-img-container">
-              <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
-          </div>
-          <div class="card-info-container">
-              <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-              <p class="card-text">${employee.email}</p>
-              <p class="card-text cap">${employee.location.state}, ${getCountry(employee.nat)}</p>
-          </div>
-        </div>
-      `
-      galleryDiv.innerHTML += employeeCard;
+    document.getElementById("search-input").addEventListener('click', function () {
+      setTimeout(function() {
+        resetAllEmployees();
+      }, 20)
     });
 
-    createModalContainer();
+    document.getElementById("search-input").addEventListener('keyup', function () {
+      resetAllEmployees();
+    });
   }
 
+  function createSearchArray (userInput) {
+  //  let searchEmployees = new Array();
+    searchEmployees = [];
+    userInput = userInput.trim().toLowerCase().replace(/  +/g, ' ');   
+
+    employees.forEach(employee => {
+      let name = employee.name.first + " " + employee.name.last;
+      fName = employee.name.first.trim().toLowerCase();
+      lName = employee.name.last.trim().toLowerCase(); //if more than one space in rest of name (ie. lilou le gall)
+
+      if ((name.search(userInput) === 0) 
+      || (fName.search(userInput) === 0)
+      || (lName.search(userInput) === 0)) {
+            searchEmployees.push(employee);
+      }
+    });
+    return searchEmployees;
+  }
+
+  function processNoResults() {
+    const divSearchContainer = document.getElementsByClassName('search-container')[0];
+    if (document.getElementsByClassName('search-error').length === 0) {
+      const divError = document.createElement('DIV');
+      divError.classList.add('search-error');
+      divError.innerHTML = '<p>Your search found no results</p>';
+      divSearchContainer.appendChild(divError);
+    }
+    createEmployeeCards(employees);
+    searchEmployees = [];
+  }
+
+  /** 
+    HELPER FUNCTIONS
+  */
+  function checkStatus(response) {
+    if (response.ok) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  }
+
+  function resetAllEmployees() {
+    if (document.getElementById("search-input").value === '') {
+      //remove 'no search result' error message if exists
+      const searchContainer = document.getElementsByClassName('search-container')[0];
+      const errorDiv = document.getElementsByClassName('search-error');
+      if (errorDiv.length) {
+        searchContainer.removeChild(errorDiv[0]);
+      }
+      
+      //Display all employees and reset searchEmployees array
+      if (searchEmployees.length > 0) {
+        createEmployeeCards(employees);
+        searchEmployees = [];
+      }
+    }
+  }
+
+  function getCardIndex(employeeArray) {
+    const targetEmail = document.getElementsByClassName("modal-text")[0].textContent;
+    return employeeArray.findIndex((employee => employee.email === targetEmail));
+  }
+
+  function processNextPrev(employees, which) {
+    indexOfCard = getCardIndex(employees);
+    if (which === "next") {
+      indexOfCard = (indexOfCard === employees.length - 1) ? 0 : (indexOfCard + 1);
+    } else {
+      indexOfCard = (indexOfCard) ? (indexOfCard - 1) : (employees.length - 1);
+    }
+    addModalInfo(employees[indexOfCard]);
+  }
+
+  function getCountry(employeeCountry) {
+    const countryArray = [
+      {abbr: "AU", country: "Australia"}, {abbr: "GB",country: "United Kingdom"},
+      {abbr: "NZ", country: "New Zealand"}, {abbr: "US",country: "United States"}
+    ];
+    return countryArray.find(country => employeeCountry === country.abbr).country;
+  }
+
+  // https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+
+  function formatPhoneNumber(phoneNumberString) {
+    if (phoneNumberString.charAt(4) === "-" || phoneNumberString.charAt(4) === ")") {
+      var cleaned = (phoneNumberString).replace(/\D/g, '')
+      var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+      if (match) {
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+      }
+    }
+    return phoneNumberString;
+  }
+
+  function formatBirthday(birthday) {
+    birthday = new Date(birthday);
+    return (birthday.getMonth() + 1) + "/" + birthday.getDate() + "/" + birthday.getFullYear();
+  }
 }); //end content loaded
